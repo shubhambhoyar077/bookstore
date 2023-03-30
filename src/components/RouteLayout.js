ff8e4d67c1a2eb9e7c869861ed91135c4e34dd3e
shubham <shubham.bhoyar077@gmail.com>
2023-03-30 13:51:20 +0530

    Create default layout for router

---
+++ src/components/RouteLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+
+export default function RouteLayout() {
+  <>
+    <header>
+      <NavBar />
+    </header>
+    <main>
+      <Outlet />
+    </main>
+  </>
+}
