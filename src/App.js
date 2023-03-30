d3429b2b3df187ef5340b798d2d2387e5175a905
shubham <shubham.bhoyar077@gmail.com>
2023-03-30 14:43:43 +0530

    Add router as per instruction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+ } from 'react-router-dom';
 import './App.css';
+
+import RouteLayout from './components/RouteLayout';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RouteLayout />}>
+      <Route index element={<BookList />} />
+      <Route path='categories' element={<Categories />} />
+    </Route>
+  ),
+);
 
 function App() {
   return (
-    
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
