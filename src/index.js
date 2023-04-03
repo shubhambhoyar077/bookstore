cd660c9c6596a875754a8e95015ad5d1fe8319d7
shubham <shubham.bhoyar077@gmail.com>
2023-04-03 15:30:10 +0530

    Fix export as per eslinter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
