8d681ead3dff1aa9dba4103ef7332c9200f59b02
shubham <shubham.bhoyar077@gmail.com>
2023-04-03 15:49:37 +0530

    Create folder structure and create book reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: booksReducer,
+  },
 });
 
 export default store;
