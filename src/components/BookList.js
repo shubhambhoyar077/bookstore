2c0a5f80f6851bdeee71bc614d5dee07bf14f09c
shubham <shubham.bhoyar077@gmail.com>
2023-04-04 19:38:03 +0530

    Add dispact in useEffect got api call

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './Book';
 import AddBook from './AddBook';
+import { getBooks } from '../redux/books/booksSlice';
 
 export default function BookList() {
   const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   return (
     <div>
       <ul className="booklist">
